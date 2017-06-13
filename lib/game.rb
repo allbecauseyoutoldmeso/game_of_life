349a90eebe91b3945909b568ad59f61f4f0cbb56
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-06-13 12:55:54 +0100

    refactor neighbours method

--- lib/game.rb
+++ lib/game.rb
@@ -13,21 +13,23 @@ class Game
     column = grid.flatten.index(cell) % size
 
     unless row == 0
-      cell_neighbours.push grid[row-1][column-1] unless column == 0
-      cell_neighbours.push grid[row-1][column]
-      cell_neighbours.push grid[row-1][column+1] unless column == size-1
+      push_cells(row-1, column, cell_neighbours)
     end
 
     cell_neighbours.push grid[row][column-1] unless column == 0
     cell_neighbours.push grid[row][column+1] unless column == size-1
 
-    unless row == size-1      
-      cell_neighbours.push grid[row+1][column-1] unless column == 0
-      cell_neighbours.push grid[row+1][column]
-      cell_neighbours.push grid[row+1][column+1] unless column == size-1
+    unless row == size-1
+      push_cells(row+1, column, cell_neighbours)
     end
 
     cell_neighbours
+  end
+
+  def push_cells(row, column, array)
+    array.push grid[row][column-1] unless column == 0
+    array.push grid[row][column]
+    array.push grid[row][column+1] unless column == size-1
   end
 
   def number_live(cell)
