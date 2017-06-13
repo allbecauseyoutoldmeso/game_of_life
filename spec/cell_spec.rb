fe95c10d0fccc2349eb3079f83cd9d4eea747760
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-06-13 19:29:21 +0100

    factor out make_live method for cell

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,10 +2,17 @@ require 'cell'
 
 describe Cell do
 
-  subject(:cell) { described_class.new(true)}
+  subject(:cell) { described_class.new(false)}
 
   describe '#live?' do
-    it 'returns true if the cell is initialized as true' do
+    it 'returns false if the cell is initialized as false' do
+      expect(cell.live?).to eq false
+    end
+  end
+
+  describe '#make_live' do
+    it 'makes the cell live' do
+      cell.make_live
       expect(cell.live?).to eq true
     end
   end
