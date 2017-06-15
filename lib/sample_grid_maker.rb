8b8d43e04384ec96abc7891f853a72dcefcdb39a
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-06-15 17:46:36 +0100

    extract make_coordinates live method for sampleGridMaker

--- lib/sample_grid_maker.rb
+++ lib/sample_grid_maker.rb
@@ -4,9 +4,9 @@ class SampleGridMaker
 
   def basic_grid
     grid = []
-    5.times do
+    6.times do
       row = []
-      5.times { row.push(Cell.new(false)) }
+      6.times { row.push(Cell.new(false)) }
       grid.push(row)
     end
     grid
@@ -14,10 +14,24 @@ class SampleGridMaker
 
   def blinker
     blinker_grid = basic_grid
-    blinker_grid[1][2].make_live
-    blinker_grid[2][2].make_live
-    blinker_grid[3][2].make_live
+    make_coordinates_live(blinker_grid, [[1,2],[2,2],[3,2]])
     blinker_grid
+  end
+
+  # def beacon
+  #   beacon_grid = basic_grid
+  #   beacon_grid[2][2].make_live
+  #   beacon_grid[2][3].make_live
+  #
+  # end
+
+  def toad
+  end
+
+  def make_coordinates_live(grid, coordinate_array)
+    coordinate_array.each do |coordinates|
+      grid[coordinates[0]][coordinates[1]].make_live
+    end
   end
 
 end
