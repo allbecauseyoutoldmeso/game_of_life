5dd047d7f9e4ffa71f70c454fe0085e7a6e443f0
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-06-15 14:47:56 +0100

    remove redundant tets

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -31,28 +31,16 @@ describe Game do
                              [cell_30, cell_31, cell_32, cell_33]]
   end
 
-  describe '#tick' do
-    it 'cells die due to underpopulation' do
-      game.tick
-      expect(game.grid[0][0].live?).to eq false
-    end
-    it 'cells die due to overpopulation' do
-      game.tick
-      expect(game.grid[1][1].live?).to eq false
-    end
-    it 'cells become live thanks to reproduction' do
-      game.tick
-      expect(game.grid[1][0].live?).to eq true
-    end
-    it 'other cells are unaffected' do
-      game.tick
-      expect(game.grid[1][3].live?).to eq false
-    end
-  end
-
   describe '#print_grid' do
     it 'prints the grid to the console' do
       expect { game.print_grid }.to output("[x][ ][ ][ ]\n[ ][x][x][ ]\n[ ][x][x][ ]\n[ ][ ][ ][ ]\n").to_stdout
+    end
+  end
+
+  describe '#tick' do
+    it 'updates the grid' do
+      game.tick
+      expect { game.print_grid }.to output("[ ][x][ ][ ]\n[x][ ][x][ ]\n[ ][x][x][ ]\n[ ][ ][ ][ ]\n").to_stdout
     end
   end
 
